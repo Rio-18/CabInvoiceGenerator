b95155f0ddd4b95d0885c7991883a31d3d2e8a2f
riya more <you@example.com>
2023-03-13 23:12:24 +0530

    CabInvoiceGenerator

---
+++ InvoiceSummary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceSummary
+    {
+        private int numberofRides;
+        private double totalFare;
+        private double averageFare;
+
+        public InvoiceSummary(int numberofRides,double totalFare)
+        {
+            this.numberofRides= numberofRides;
+            this.totalFare= totalFare;
+            this.averageFare=this.totalFare/this.numberofRides;
+        }
+    }
+}
