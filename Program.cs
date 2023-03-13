d625c32b9a34d067cfca9b450ee352de0d394209
riya more <you@example.com>
2023-03-13 23:09:37 +0530

    CabInvoiceGenerator

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to cab invoice generator");
+            InvoiceGeneratot invoice = new InvoiceGeneratot(RideType.NORMAL);
+
+            double fare = invoice.CalculateFare(2.0, 5);
+            Console.WriteLine(fare);
+        }
+    }
+
+}
